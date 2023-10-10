2f6032925e8fd4de51c7a70c60919f494b24f0fe
Parth Purvesh <parth29dec@gmail.com>
2023-10-10 11:40:04 +0530

    Tests passed

--- src/main/java/Twofer.java
+++ src/main/java/Twofer.java
@@ -1,5 +1,7 @@
 public class Twofer {
     public String twofer(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String cName = name == null ? "you" : name;
+        return "One for " + cName + ", one for me.";
     }
 }
